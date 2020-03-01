5a8dfeadad1f78ceec2a97dde1d4e12bfbc31a8e
akshitagupta15june <akshitagupta15june@gmail.com>
2020-03-02 00:29:39 +0530

    welcome to 100 days of code

---
+++ welcome.py
@@ -0,0 +1,7 @@
+Python 2.7.17 (default, Nov  7 2019, 10:07:09) 
+[GCC 9.2.1 20191008] on linux2
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: /home/akshita/100code/.git/welcome.py ===============
+welcome to 100 days of code
+>>> 
