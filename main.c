b2c43c1c11d9ae98a146a54efa687359852b19a7
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2020-03-19 11:09:28 +0530

    Stack using Linklist

--- main.c
+++ main.c
@@ -1,50 +1,75 @@
-#include <stdio.h>
-#define n 5
-int top=-1;
-int stack[n];
-void push()
+#include<stdio.h>
+#include<malloc.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *top=NULL;
+struct node *push();
+struct node *pop();
+struct node *peak();
+struct node *display();
+
+struct node *push()
+{
+    struct node *newnode;
+    newnode=(struct node*)malloc(sizeof(struct node));
+    printf("\n Enter data\t");
+    scanf("%d",&newnode->data);
+    newnode->next=top;
+    top=newnode;
+}
+struct node *pop()
 {
     int item;
-    printf("\n Enter data to be entered in stack");
-    scanf("%d",&item);
-    if(top==n-1)
+    struct node *temp;
+    temp=top;
+    item=temp->data;
+    if(top==NULL)
     {
-        printf("\n Stack Overflow");
+        printf("\n Stack Empty");
     }
     else
     {
-       top++;
-       stack[top]=item;
+    top=top->next;
+    free(temp);
+    printf("\ndata which is deleted is %d",item);
     }
 }
-void pop()
+struct node *peak()
 {
-    int temp;
-    temp=stack[top];
-    top--;
-    printf("Item deleted is %d",temp);
-}
-void peek()
-{
-    int temp;
-    temp=stack[top];
-    printf("%d",temp);
-    
-}
-void display()
-{
-    for(int i=top;i>=0;i--)
+    if(top==NULL)
     {
-        printf("%d",stack[i]);
+        printf("\n Stack Empty");
+    }
+    else
+    {
+        printf(" %d",top->data);
+        
+    }
+}
+struct node *display()
+{
+    struct node *temp;
+    temp=top;
+    if(top==NULL)
+    {
+        printf("\n Stack Empty");
+    }
+    
+    while(temp!=NULL)
+    {
+        printf("\n%d ",temp->data);
+        temp=temp->next;
     }
 }
 int main()
 {
     int ch;
-    
     do
     {
-        printf("Enter\n1:push\n2:pop\n3:peek\n4:display\n5:(0) to exit\t");
+        printf("Enter\n1:push\n2:pop\n3:peak\n4:display\n5:(0) to exit\t");
         scanf("%d",&ch);
         switch(ch)
         {
@@ -55,7 +80,7 @@ int main()
             pop();
             break;
             case 3:
-            peek();
+            peak();
             break;
             case 4:
             display();
